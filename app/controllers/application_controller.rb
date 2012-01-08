89d6e15db1f8982989e5c5245298801e99318212
Formalsky Iliya <i.formalsky@scalaxy.ru>
2012-01-08 13:01:20 +0400

    internalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_locale
+
+  def set_locale
+    # update session if passed
+    session[:locale] = params[:locale] if params[:locale]
+
+    # set locale based on session or default 
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
 end
